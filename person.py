f2ed5182416aa61a0128f1e6c3da80739fb776de
mnsinclair <msin553@aucklanduni.ac.nz>
2022-05-25 11:57:55 +1200

    Delete obsolete functions, add special action handlers

--- person.py
+++ person.py
@@ -43,6 +43,7 @@ class Person:
         self.location_state = location_state
         # Add self to the collection of people in the room
         self.location_state.add_person(self)
+
         # Initialise the conversation partner
         self.conversation_partner = conversation_partner
 
@@ -151,62 +152,21 @@ class Person:
 
         return action
 
-    def take_action(self, type=None):
-        """This funciton selects the action to take next and returns it."""
-        # If no type of action is specified, select one.
-        if type == None:
-            type = self.select_action_type()
+    # Methods to handle special non-conversation actions
+    def leave_conversation(self):
+        """This function handles the special case of a person leaving a conversation."""
+        assert self.conversation_partner != None, "No conversation partner to leave"
+        if self.conversation_partner.has_conversation_partner():
+            self.conversation_partner.leave_conversation()
+        self.conversation_partner = None
 
-        if type == "verbal":
-            verbal_action = self.select_verbal_action()
-            return verbal_action
-
-        elif type == "non-verbal":
-            non_verbal_action = self.select_non_verbal_action()
-            return non_verbal_action
-
-        elif type == "move":
-            to_room = self.select_next_room()
-            return self.move_to(to_room)
-
-        else:
-            print(type)
-
-    def select_non_verbal_action(self):
-        # TODO implement non-verbal action selection. This could use any of the below attributes. For now, just return an empty action.
-        # self.personality_scales
-        # self.social_state
-        # self.emotional_state
-        # self.location_state (Any of the properties of the location state (current room) can be used to influence the action taken.)
-        return Action(name="DefaultNonVerbalAction", taker=self, description="takes a non verbal action")
-
-    def select_verbal_action(self):
-        # TODO implement verbal action. This could use any of the below attributes. For now, just return a default action.
-        # self.personality_scales
-        # self.social_state
-        # self.emotional_state
-        # self.location_state (Any of the properties of the location state (current room) can be used to influence the action taken.)
-        return Action(name="DefaultVerbalAction", taker=self, description="takes a verbal action")
-
-    def select_next_room(self):
-        # TODO implement how next room is selected. Could include things like people in room, whether food available etc. For now, just pick a random adjacent room.
-        ajacent_rooms = self.location_state.get_adjacent_rooms()
-        return np.random.choice(list(ajacent_rooms))
-
-    def move_to(self, room):
-        from_room = self.location_state
+    def move_to_room(self, room):
+        """This function handles the special case of a person moving to a new room."""
+        from_room = self.get_location_state()
         to_room = room
         # Remove self from the current room
         from_room.remove_person(self)
         # Update own location state to the new room
-        self.location_state = to_room
+        self.set_location_state(to_room)
         # Add self to the new room 'people set'
         to_room.add_person(self)
-
-        return Action(name="movement", taker=self, description=f"leaves the {from_room.get_name()} and enters the {to_room.get_name()}")
-
-    def select_action_type(self):
-        # TODO implement a function to select what type of action to take next. For now, just select a random action type.
-        action_types = list(self.action_type_probs.keys())
-        action_probs = list(self.action_type_probs.values())
-        return np.random.choice(action_types, p=action_probs)
