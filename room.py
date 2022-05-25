53196db2113a65a70a699624491c6b3faab0f42e
mnsinclair <msin553@aucklanduni.ac.nz>
2022-05-25 12:57:00 +1200

    Simplified "Room" to reflect discussion

--- room.py
+++ room.py
@@ -1,24 +1,49 @@
-class Room:
-    def __init__(self, name, base_loudness=0, intended_occupancy=5, has_food=False, has_drinks=False):
-        self.name = name  # The name of the room
-        self.people = set()  # The set of people currently in the room
-        # How loud the room is with no-one in it. 0 is silent.
-        self.base_loudness = base_loudness
-        # How many people can comfortably fit in the room before it is crowded.
-        self.intended_occupancy = intended_occupancy
-        # Whether the room has food available
-        self.has_food = has_food
-        # Whether the room has food available
-        self.has_drinks = has_drinks
-        # The set of rooms that are adjacent to this room. Empty to start with.
-        self.adjacent_rooms = set()
+from __future__ import annotations
+import numpy as np
+from action import Action
+from typing import List, Set
+import person
 
-    def add_adjacent_room(self, room):
-        # Add an adjacent room to the set of adjacent rooms
-        self.adjacent_rooms.add(room)
-        # Adjacency is reflexive (if A is adjacent to B, then B is adjacent to A)
-        if self not in room.get_adjacent_rooms():
-            room.add_adjacent_room(self)
+
+class Room:
+    def __init__(self, name: str, adjacent_rooms: Set[Room] = set(), people_in_room: Set[person.Person] = set()):
+        assert(type(name) == str), f"{name} is not a string"
+        assert(np.array([type(room) == Room for room in adjacent_rooms]).all(
+        )), f"{adjacent_rooms} is not a set of rooms"
+        assert(np.array([type(person) == person.Person for person in people_in_room]).all(
+        )), f"{adjacent_rooms} is not a set of people"
+
+        self.__name = name  # The name of the room
+        self.__people = set()  # The set of people currently in the room
+        # The set of rooms that are adjacent to this room. Empty to start with.
+        self.__adjacent_rooms = adjacent_rooms
+
+    def __repr__(self):
+        return f"""
+        ===========================
+        The {self.name}:
+        ===========================
+        People: {[str(x) for x in self.people]}
+        Num people: {len(self.people)}
+        Adjacent to: {self.get_adjacent_room_names()}
+        """
+
+    def get_name(self):
+        """Returns room name"""
+        return self.name
+
+    def add_person(self, person):
+        """Adds a person to the room"""
+        assert(
+            person not in self.people), f"{person} is already in {self.name}"
+        assert(type(person) == person.Person), f"{person} is not a person"
+        self.people.add(person)
+
+    def remove_person(self, person):
+        self.people.remove(person)
+
+    def has_person(self, person):
+        return person in self.people
 
     def get_people(self):
         return self.people
@@ -30,38 +55,13 @@ class Room:
     def get_adjacent_room_names(self):
         return [x.get_name() for x in self.get_adjacent_rooms()]
 
-    def get_current_loudness(self):
-        # Todo define the relationship between people & loudness (maybe speaking volume attribute in the Person class)
-        return self.base_loudness + len(self.people)
-
-    def get_current_crowdedness(self):
-        # Todo define the relationship between number of people & how crowded the room feels (maybe speaking volume attribute in the Person class)
-        if self.intended_occupancy == 0:
-            # Avoid a divide by zero error
-            return float("inf")
-        return len(self.people) / self.intended_occupancy * 100
-
-    def __repr__(self):
-        return f"""
-        ===========================
-        The {self.name}:
-        ===========================
-        People: {[str(x) for x in self.people]}
-        Num people: {len(self.people)}
-        Base Loudness: {self.base_loudness}
-        Current Loudness: {self.get_current_loudness()}
-        Intended Capacity: {self.intended_occupancy}
-        Crowdedness (% max capacity): {self.get_current_crowdedness()}
-        Has food: {self.has_food}
-        Has drinks: {self.has_drinks}
-        Adjacent to: {self.get_adjacent_room_names()}
-        """
-
-    def get_name(self):
-        return self.name
-
-    def add_person(self, person):
-        self.people.add(person)
-
-    def remove_person(self, person):
-        self.people.remove(person)
+    def add_adjacent_room(self, other_room):
+        """Adds an adjacent room to this room"""
+        assert(type(other_room) == Room), f"{other_room} is not a room"
+        assert(
+            other_room not in self.adjacent_rooms), f"{other_room} is already adjacent to {self.name}"
+        # Add an adjacent room to the set of adjacent rooms
+        self.adjacent_rooms.add(other_room)
+        # Adjacency is reflexive (if A is adjacent to B, then B is adjacent to A)
+        if self not in other_room.get_adjacent_rooms():
+            other_room.add_adjacent_room(self)
